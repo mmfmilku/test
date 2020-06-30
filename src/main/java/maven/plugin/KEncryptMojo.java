32a7f6e8c1b9b1002d1df2f8e3d937d92898828c
milku <crino1cirno@gmail.com>
2020-06-30 17:52:11 +0800

    jar包加密mojo实现实验

--- src/main/java/maven/plugin/KEncryptMojo.java
+++ src/main/java/maven/plugin/KEncryptMojo.java
@@ -3,15 +3,24 @@ package maven.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import util.FileUtil;
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
+import java.util.zip.ZipEntry;
 
 @Mojo(name = "kencrypt", defaultPhase = LifecyclePhase.PACKAGE)
 public class KEncryptMojo extends AbstractMojo {
-
-    @Parameter(property = "project.basedir")
-    private String baseDir;
 
     @Parameter(property = "project.build.finalName")
     private String finalName;
@@ -25,14 +34,55 @@ public class KEncryptMojo extends AbstractMojo {
     @Parameter(property = "project.build.outputDirectory")
     private String outputDir;
 
+    @Parameter(property = "file.separator")
+    private String fileSeparator;
+
+    @Parameter
+    private List<String> encryptDirs;
+
+    private Log logger = getLog();
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info(baseDir + "," + finalName);
-        System.out.println("encrypt mojo here!");
-        System.out.println("baseDir:" + baseDir);
-        System.out.println("finalName:" + finalName);
-        System.out.println("packaging:" + packaging);
-        System.out.println("buildDir:" + buildDir);
-        System.out.println("outputDir:" + outputDir);
+        String filePath = buildDir + fileSeparator + finalName + "." + packaging;
+        logger.info("--- start encrypt your package file " + filePath + " ---");
+
+        File f = new File(filePath);
+        if (!f.exists()) {
+            logger.error("package file not exists:" + filePath);
+            return;
+        }
+
+        JarFile jarFile = null;
+//        InputStream in = null;
+//        JarInputStream jarIn = null;
+        try {
+            jarFile = new JarFile(f);
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while (entries.hasMoreElements()) {
+                System.out.println("-----------------");
+                JarEntry jarEntry = entries.nextElement();
+                System.out.println(jarEntry.getName());
+                if (jarEntry.isDirectory())
+                    continue;
+                InputStream inputStream = jarFile.getInputStream(jarEntry);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null)
+                    System.out.println(line);
+                reader.close();
+            }
+        } catch (IOException e) {
+            logger.error(e);
+        } finally {
+            if (jarFile != null) {
+                try {
+                    jarFile.close();
+                } catch (IOException e) {
+                    logger.error(e);
+                }
+            }
+        }
+
     }
 }
